8bfcadf2d51f1f2b07aa102ee456940658ea8d53
Tomasz Oraczewski <tomasz.oraczewski@silvair.com>
2020-12-10 12:53:25 +0100

    Initial view controller

--- bt-adapter-test/ViewController.swift
+++ bt-adapter-test/ViewController.swift
@@ -8,12 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .darkGray
+        view.addSubview(label)
     }
-
-
 }
-
