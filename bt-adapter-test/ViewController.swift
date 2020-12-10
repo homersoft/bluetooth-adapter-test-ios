554f72ed9ca17b323bd3a8b48800c6b830e84260
Tomasz Oraczewski <tomasz.oraczewski@silvair.com>
2020-12-10 08:58:57 +0100

    Initial Commit

---
+++ bt-adapter-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bt-adapter-test
+//
+//  Created by Tomasz Oraczewski on 10/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
